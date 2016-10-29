894f7a759675745ab88403f62ff139303bac1d54
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2016-10-29 18:57:25 +0800

    Initial Commit

---
+++ NFSpotifyAuthenticatorSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFSpotifyAuthenticatorSample
+//
+//  Created by Neil Francis Hipona on 10/29/16.
+//  Copyright © 2016 Neil Francis Hipona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
